a65921b901d05b4431e3b3ddaa243fc346ba4bf8
Bekzat707 <bekzatsaparbekov070@gmail.com>
2024-03-27 23:12:54 +0600

    Create clock.py

---
+++ clock/clock.py
@@ -0,0 +1,57 @@
+import pygame
+import datetime
+
+pygame.init()
+
+screen = pygame.display.set_mode((1400,1050))
+pygame.display.set_caption("Часы ")
+icon = pygame.image.load("photo.jpg")
+pygame.display.set_icon(icon)
+fps = 24
+background = pygame.image.load("mainclock.png")
+
+clock = pygame.time.Clock()
+
+minhand = pygame.image.load("rightarm.png")
+sechand = pygame.image.load("leftarm.png")
+
+minrect = minhand.get_rect()
+minrect.center = ((700,525))
+
+secrect =sechand.get_rect()
+secrect.center = (700,525)
+
+currenttime = datetime.datetime.now()
+secvalue = currenttime.second
+minvalue = currenttime.minute
+
+run = True
+
+while run:
+    for event in pygame.event.get():
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            run = False
+            pygame.quit()
+            exit()
+
+    screen.blit(background, (0,0))
+    genminhand = pygame.transform.rotate(minhand, -1*((6*minvalue)%360))
+    genminrect = genminhand.get_rect()
+    genminrect.center = minrect.center
+
+    screen.blit(genminhand,genminrect)
+
+    gensechand = pygame.transform.rotate(sechand, -1*((6*secvalue+180)%360))
+    gensecrect = gensechand.get_rect()
+    gensecrect.center = secrect.center
+
+    screen.blit(gensechand, gensecrect)
+
+    currenttime = datetime.datetime.now()
+    secvalue = currenttime.second
+    minvalue = currenttime.minute
+
+    pygame.display.update()
+    clock.tick(fps)
+
+pygame.quit()
